8704829f1af166c9895335ce5e65a1a671bc29a4
Godswillben <amogugodswill99@gmail.com>
2023-04-01 22:14:36 +0100

    complex mutex example

--- mutex1/main.go
+++ mutex1/main.go
@@ -5,39 +5,51 @@ import (
 	"sync"
 )
 
-var msg string
 var wg sync.WaitGroup
 
-func updatedMessage(s string){
-	defer wg.Done()
-	msg = s
+type Income struct {
+	Source string
+	Amount int
 }
 
-func main() {
-	msg = "Hello, world!"
+func main(){
+	// variable for banking
+	var bankBalance int
+	var balance sync.Mutex
 
-	wg.Add(2)
-	go updatedMessage("Hello World")
-	go updatedMessage("Hello, cosmos!")
+	// print out starting values
+	fmt.Printf("Intial bank balance: $%d.00", bankBalance)
+	fmt.Println()
+
+	// define weekly revenue
+	incomes := []Income{
+		{Source: "Main Income", Amount: 500},
+		{Source: "Gifts", Amount: 10},
+		{Source: "Part time job", Amount: 50},
+		{Source: "Investments", Amount: 100},
+	}
+
+	wg.Add(len(incomes))
+
+	// loop through 52 weeks and print out how much is made
+	for i, income := range incomes{
+		go func(i int, income Income){
+			defer wg.Done()
+
+			for week := 1; week <= 52; week++ {
+				balance.Lock()
+				temp := bankBalance
+				temp += income.Amount
+				bankBalance = temp
+				fmt.Println("On week %d, you earned $%d.00 from %s\n", week, income.Amount, income.Source)
+				balance.Unlock()
+			}
+		}(i, income)
+	}
+
 	wg.Wait()
 
-	fmt.Println(msg)
+	// print out final balance
+	fmt.Printf("Final bank balance: $%d.00", bankBalance)
+	fmt.Println()
 }
-
-// func updatedMessage(s string, m *sync.Mutex){
-// 	defer wg.Done()
-// 	msg = s
-// }
-
-// func main() {
-// 	msg = "Hello, world!"
-
-// 	var mutex sync.Mutex
-
-// 	wg.Add(2)
-// 	go updatedMessage("Hello World", &mutex)
-// 	go updatedMessage("Hello, cosmos!", &mutex)
-// 	wg.Wait()
-
-// 	fmt.Println(msg)
-// }
